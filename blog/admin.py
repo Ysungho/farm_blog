80de45439c4ecee18a2fdcd96f566803878ee59b
Ysungho <ysh01022320@naver.com>
2024-03-26 10:38:31 +0900

    post 모델을 생성하고, 시간 설정

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+"""django 관리자 페이지 설정"""
 
+from django.contrib import admin
+from .models import Post
 # Register your models here.
+
+"""관리자 페이지에 Post 모델 등록"""
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
+"""블로그의 핵심인 포스트의 형태를 정의하는 Post 모델을 만듬"""
+"""제목(title), 내용(content), 작성일(created_at), 작성자 정보(author)가 필요"""
+
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+    title=models.CharField(max_length=20) #제목: 최대 20글자
+    content=models.TextField() #글 내용
+
+    # 포스트 작성
+    created_at=models.DateTimeField(auto_now_add=True) # 작성시간 : 현재 시간 자동 입력
+    # auto_now_add=True 처음 레코드가 설정될 때 현재 시각이 자동으로 저장
+
+    # 포스트 수정
+    updated_at=models.DateTimeField(auto_now=True)
+    #작성자는 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
