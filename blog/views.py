335cd44a9beeaf85ce0319493ec2b62cf6ed4f12
Ysungho <ysh01022320@naver.com>
2024-03-26 11:24:55 +0900

    CBV로 블로그 포스트 목록 만들기\

--- blog/views.py
+++ blog/views.py
@@ -2,28 +2,34 @@
 
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+
+"""PostList를래스를 ListView클래스를 상속해서 만듬"""
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    #template_name = 'blog/index.html'
+    # html파일명을 _list.html로 변경하거나 template_name='blog/index.html'로 설정하면 됨
 
 
-# Create your views here.
-
-def index(request):
-    # Post.objects.all()로 모든 Post 레코드를 가져와 posts에 저장
-    # .order_by('.pk')는 pk값의 역순으로 정렬=> 가장 최근에 만든 포스트 부터 조회됨
-    posts = Post.objects.all().order_by('-pk')
-
-    # render() 함수 안에 posts를 딕셔너리 형태로 추가
-    return render(
-        # index.html에서 posts를 이용가능하게 만들어줌
-        request,
-        'blog/index.html',
-        {
-            # index.html에서 {%for p in posts%} 형식으로 사용하면 {{p.title}} 이렇게 사용가능
-            # {{}}은 단순 변수임
-            # 반드시 {%endfor%}로 마무리 해야함
-
-            'posts': posts,
-        }
-    )
+# def index(request):
+#     # Post.objects.all()로 모든 Post 레코드를 가져와 posts에 저장
+#     # .order_by('.pk')는 pk값의 역순으로 정렬=> 가장 최근에 만든 포스트 부터 조회됨
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     # render() 함수 안에 posts를 딕셔너리 형태로 추가
+#     return render(
+#         # index.html에서 posts를 이용가능하게 만들어줌
+#         request,
+#         'blog/index.html',
+#         {
+#             # index.html에서 {%for p in posts%} 형식으로 사용하면 {{p.title}} 이렇게 사용가능
+#             # {{}}은 단순 변수임
+#             # 반드시 {%endfor%}로 마무리 해야함
+#
+#             'posts': posts,
+#         }
+#     )
 
 
 def single_post_page(request, pk):
